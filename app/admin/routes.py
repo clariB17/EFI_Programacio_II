8c3413c3f6c09a3fccda79b3e2b63db35bba61d9
vabklb <v.barilin@itecriocuarto.org.ar>
2021-11-15 20:44:20 -0300

    unitest

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -6,15 +6,6 @@ from app.models import Genero, Libro, Genero, Pais, Autor, Idioma
 from . import admin_bp
 from .forms import Libros_upload, Autores_upload
 from werkzeug.utils import secure_filename
-<<<<<<< HEAD
-from extension_file import libro_permitido, imagen_permitida
-import os
-
-FOLDER_LIBRO = os.path.abspath('app/static/libros')
-FOLDER_IMAGEN_LIBRO = os.path.abspath('app/static/img_libro')
-FOLDER_IMAGEN_AUTOR = os.path.abspath('app/static/img_autor')
-
-=======
 from extension_file import imagen_permitida, libro_permitido
 import os
 
@@ -22,7 +13,6 @@ import os
 FOLDER_LIBRO = os.path.abspath('app/static/libros')
 FOLDER_IMAGEN_LIBRO = os.path.abspath('app/static/img_libro')
 FOLDER_IMAGEN_AUTOR = os.path.abspath('app/static/img_autor')
->>>>>>> b1f31ff0ea1df9b933757b4cd221104148494228
 
 @admin_bp.route("/admin/libro_upload/", methods=['GET', 'POST'])
 @login_required
